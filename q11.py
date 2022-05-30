f2eb15b391005520bec4ce3b959acf9af5b1d19f
Antun <antun.jerkovic@gmail.com>
2022-05-30 05:15:15 +0200

    Prvi_Tidra_Commit

---
+++ q11.py
@@ -0,0 +1,17 @@
+d = {
+    "food": {
+        "rice":
+            {
+                "weight": 30.1,
+                "taste": "good",
+                "forms": ["boiled"]
+            },
+        "banana":
+            {
+                "weight": 19,
+                "taste": "excellent",
+            }
+        }
+}
+
+print(d['food']['banana']['taste'])
