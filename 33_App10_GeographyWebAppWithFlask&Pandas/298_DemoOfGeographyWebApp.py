2e038bc2a68d2f1e4097093848f4f2f79224141f
Antun <antun.jerkovic@gmail.com>
2022-07-05 20:28:48 +0200

    App10Gotov

---
+++ 33_App10_GeographyWebAppWithFlask&Pandas/298_DemoOfGeographyWebApp.py
@@ -0,0 +1,14 @@
+# 7'31''
+# Opet jedan stariji video
+# Specificnost ove-zadnje App je da se ona gradi u formi projekta!?
+# Trebali bi dobiti nove skillse i sasvim nice app za portofolio
+# Ali napraviti je sam bilo bi vrlo, vrlo taf osim za genija!
+# Vi bi trebali moci do oko 50% ito je super. Nemojte odmah pitati kad zapnete, probajte popit nesto i osvjeziti se!
+# Probajte napisati na papir kod i korake koje namjeravate: 1.skicirati cilj isl, 2.napisati scriptu koja cita datas i daje output, 3. Strukturirajte Flask i 4. Implementacija korak po korak
+# Gledanje raznih izvora je ok - ja to stalno radim!
+#todo Konacno idemo na demo same app
+# App pocinje dostavljanjem csv-fajla sa adresom itd
+# User uploada file sa choose-buttonom, Submita i pojavljuje se tablica ispod! sa pridodanim Latitude i Longitude koje je backend dio appa izracunao iz adrese. Ispod je button "Download" kojim istu tablicu mozemo dldati!
+# Neke adrese nisu razrijesene pa se u tablicama nalazi prazno tj NaN
+# U slucaju da user uploada (pokusa) fajl bez adrese javlja se greska ispod umjesto tabele koja upozorava ali se app ne rusi! Znaci Error handling!
+# Nemojte se iznenaditi ako moje rijesenje nije kao vase! Ima raznih puteva do vrha!
---
+++ 33_App10_GeographyWebAppWithFlask&Pandas/299_SolutionPart1.py
@@ -0,0 +1,16 @@
+# 16'21''
+# 16'21
+# Objasnjenje pocinje slikom mapa i fajlova projekta i izjavom da prilozena mapa jest finalna verzija tog programa. No postoje razlike onog sto vidim na slici videa i onog sto mapa /298 app10-geocoder donosi.
+# mapa donosi 3 podmape i 1 app.py fajl dok video pocinje sa 4 app1,2,3,4.py fajla i 4 mape(static, templates, uploads i virtual) Ard ce to objasniti i pokazati i kod svakog razvojnog fajl tako da pravim razvojnu verziju "App10_eval"
+# prva stvar koju je napravio je stvaranje static-folder structure (4 nazvane mape od kojih su static i templates standardi za Flask appse)
+# uploads mapa je za usere za download
+# mapa virtual je dir virtualnog env kako se onda radilo sa kdom:
+#? python -m virtualenv virtual
+# i mapa virtal imala je fresh copy of python, pip itd. Za naglasiti je da je u finalnoj verziji vise nema!!!
+# potom je instalirao flask i pandas i geopy u virtual mapu virtenva
+# potom se stvaraju interfacei kroz fajlove index.html i download.html. Prvo obicni html bez {} tagova! Vidi u gotovu verziju! Takodjer na pocetku nije imao <div> tag sa {}. 
+# Potom je isao u static i kreirao main.css te oblikovao tagove templates-fajlova! Nije se zelio baviti objasnjavanjem css fajla jer je selfexplanatory
+# zatim se vraca u index.html i referencira u head-tagu na /static/main.css
+# malo disgresije: neki developeri startaju sa FE a neki s BE kraja
+# nakon napravljenog interfejsa kreira 1.2.3.4. verziju app-fajla - i ja napravio! Na zalost, svaka ima u sebi gresaka tako da sam ih morao komati!
+# slusajuci arda ipak shavcam da to ne ide! pocetnici moraju ici prema kompleksnosti polagano. neide drugacije!!!
